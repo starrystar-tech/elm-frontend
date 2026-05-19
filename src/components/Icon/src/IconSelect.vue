ef6e35cc70aed2ea9dce66796e1b169364c67830
lixiaobin <lixiaobin@baijiayun.com>
2026-05-19 17:40:38 +0800

    feat: 售后icon

--- src/components/Icon/src/IconSelect.vue
+++ src/components/Icon/src/IconSelect.vue
@@ -51,7 +51,7 @@ const localSvgIcons = ref<string[]>([])
 onMounted(() => {
     // 这里我们手动添加一些本地SVG图标作为示例
     // 实际项目中，你可以通过扫描src/assets/svgs目录来获取所有SVG图标
-    const localIcons = ['menu-crm', 'menu-gzh', 'wework']
+    const localIcons = ['menu-crm', 'menu-gzh', 'wework', 'aftersales']
 
     localSvgIcons.value = localIcons
     // 将本地图标添加到iconList中
